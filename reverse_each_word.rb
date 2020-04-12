f1f65803671c559cc8dd8380e1585caf2dff3eb0
Andrew Bourgeois <andrew@olympicmedia.com>
2020-04-12 04:24:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def reverse_each_word(sentence)
-  reversed_sentence = sentence.split(,)
+  reversed_sentence = sentence.split(" ")
   binding.pry
   sentence.each do |word|
     word.reverse
