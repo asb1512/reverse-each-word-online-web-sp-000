48c64ca87d53863c7989938731528ba5d292c011
Andrew Bourgeois <andrew@olympicmedia.com>
2020-04-12 04:50:35 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def reverse_each_word(sentence)
   sentence_to_array = sentence.split()
-  reversed_array = sentence_to_array.map do |word|
+  reversed_array = sentence_to_array.collect do |word|
     word.reverse
   end
   reversed_array.join(" ")
